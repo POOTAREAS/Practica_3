3c77a1bdb74832f0fa4f58c359b70588047558ea
EdgarSaul0309 <6.bautistabenitezedgarsaul@gmail.com>
2025-09-10 00:34:31 -0600

    Reto Token

--- Reto/Formulageneral.java
+++ Reto/Formulageneral.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class chicharronera {
+public class Formulageneral {
     public static void main(String[] args) {
         ArrayList<Integer> lista = new ArrayList<Integer>();
         for(String valores:args){
